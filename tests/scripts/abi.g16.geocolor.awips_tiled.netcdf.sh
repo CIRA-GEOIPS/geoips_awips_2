cfa75491d03b91a03a9ed103de93054220daaa7f
coleman-m <coleman.mcclelland@colostate.edu>
2025-09-30 16:32:10 -0600

    Trying to update scripts to use self_register. Unsuccessful so far

--- tests/scripts/abi.g16.geocolor.awips_tiled.netcdf.sh
+++ tests/scripts/abi.g16.geocolor.awips_tiled.netcdf.sh
@@ -5,11 +5,10 @@
 
 geoips run single_source $GEOIPS_TESTDATA_DIR/test_data_abi/data/goes16_20200918_1950/* \
     --reader_name abi_netcdf \
-    --resampled_read \
     --product_name GeoColor \
     --filename_formatter awips_tiles_fname \
     --output_formatter awips_tiled \
-    --sector_list goes_east
+    --reader_kwargs '{ "self_register" : "LOW" }'
 retval=$?
 
 exit $retval
