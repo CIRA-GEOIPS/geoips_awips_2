998bb4e833956182ddac3585f31ca6d251b8fb14
coleman-m <coleman.mcclelland@colostate.edu>
2025-09-30 16:31:44 -0600

    Adding logic to support GeoColor filenames; copied logic from Deb's fortran code

--- geoips_awips_2/plugins/modules/filename_formatters/awips_tiles_fname.py
+++ geoips_awips_2/plugins/modules/filename_formatters/awips_tiles_fname.py
@@ -8,7 +8,7 @@ from ipdb import set_trace as shell
 
 import logging
 
-from os.path import join as pathjoin
+from pathlib import Path
 
 from geoips.filenames.base_paths import PATHS as GPATHS
 from datetime import datetime, timezone
@@ -19,17 +19,18 @@ interface = "filename_formatters"
 family = "xarray_area_product_to_filename"
 name = "awips_tiles_fname"
 
-SOURCE_NAME_MAPPING = {
-    "abi": "ABI",
-    "ahi": "HFD",
-    "fci": "MFD",
-    "seviri": "MFD"
-}
+# Existing MAPPINGS remain
+SOURCE_NAME_MAPPING = {"abi": "ABI", "ahi": "HFD", "fci": "MFD", "seviri": "MFD"}
+PLATFORM_NAME_MAPPING = {"goes-17": "WFD", "goes-16": "EFD", "himawari-8": "EFD"}
 
-PLATFORM_NAME_MAPPING = {
-    "goes-17": "WFD",
-    "goes-16": "EFD",
-    "himawari-8": "EFD"
+# New mapping for GeoColor RGB channels (extendable if needed)
+COLOR_MAPPING = {
+    "red": "R",
+    "green": "G",
+    "blue": "B",
+    "geocolor_r": "R",
+    "geocolor_g": "G",
+    "geocolor_b": "B",
 }
 
 
@@ -38,38 +39,39 @@ def call(
     area_def,
     product_name,
     output_type=".nc",
-    basedir=pathjoin(GPATHS["GEOIPS_OUTDIRS"]),
+    basedir=Path(GPATHS["GEOIPS_OUTDIRS"]),
     extra_field=None,
 ):
-    """Create Filenames for AWIPS Tiles.
+    """Create Filenames for AWIPS and GeoColor Tiles.
 
-    Parameters
-    ----------
-    xarray_obj : xarray.Dataset
-    area_def : AreaDefinition
-    product_name : str
-    output_type : str
-    basedir : str
-    extra_field : str
-
-    Returns
-    -------
-    str
+    Detects product type and dispatches to the correct filename assembler.
     """
-    platform = PLATFORM_NAME_MAPPING[xarray_obj.platform_name]
-    source = SOURCE_NAME_MAPPING[xarray_obj.source_name]
-    product = product_name.upper()
+    product_lower = product_name.lower()
+    platform = PLATFORM_NAME_MAPPING.get(xarray_obj.platform_name, "UNK")
+    source = SOURCE_NAME_MAPPING.get(xarray_obj.source_name, "UNK")
 
-    return assemble_awips_tiles_fname(
-        basedir=basedir,
-        source_name=source,
-        platform_name=platform,
-        product_name=product,
-        start_datetime=xarray_obj.start_datetime,
-        dt_format="%Y%j%H%M%S",
-        extension=output_type,
-        creation_time=datetime.now(timezone.utc),
-    )
+    if "geocolor" in product_lower:
+        # Use Fortran-style GEOC naming
+        fname = assemble_geocolor_fname(
+            basedir=basedir,
+            product_name=product_name,
+            start_datetime=xarray_obj.start_datetime,
+            color_key=product_lower,  # used for COLOR_MAPPING lookup
+        )
+    else:
+        # Default AWIPS-style filename
+        fname = assemble_awips_tiles_fname(
+            basedir=basedir,
+            source_name=source,
+            platform_name=platform,
+            product_name=product_name.upper(),
+            start_datetime=xarray_obj.start_datetime,
+            dt_format="%Y%j%H%M%S",
+            extension=output_type,
+            creation_time=datetime.now(timezone.utc),
+        )
+
+    return fname
 
 
 def assemble_awips_tiles_fname(
@@ -82,54 +84,47 @@ def assemble_awips_tiles_fname(
     extension=".nc",
     creation_time=datetime.now(timezone.utc),
 ):
-    """Produce output product path using product / sensor specifications.
-
-    Parameters
-    ----------
-    basedir : str
-         base directory
-    source_name : str
-        Name of source (sensor)
-    platform_name : str
-        Name of platform (satellite)
-    product_name : str
-        Name of product
-    start_datetime : datetime.datetime
-        Start time of data used to generate product
-    dt_format : str, default="%Y%m%d.%H%M"
-        Format used to display start_datetime within filename
-    extension : str, default=".nc"
-        File extension, specifying type.
-    creation_time : datetime.datetime, default=None
-        Include given creation_time of file in filename
-        If None, do not include creation time.
-
-    Returns
-    -------
-    str
-        full path of output filename of the format:
-          <basedir>/OR_<sensor_name>_<data_provider>_<platform_name>_
-          surface_winds_<YYYYMMDD_HHMM>
-    """
-    fname = "-".join(
-        [
-            source_name,
-            product_name,
-            "T{tilenum}"
-        ]
-    )
-
+    """Produce AWIPS output product path."""
+    fname = "-".join([source_name, product_name, "T{tilenum}"])
     fname = "_".join(
         [
             "OR",
             fname,
             platform_name,
             f"s{start_datetime.strftime(dt_format)}",
-            f"c{creation_time.strftime(dt_format)}"
+            f"c{creation_time.strftime(dt_format)}",
         ]
     )
+    if extension:
+        fname += extension
 
-    if extension is not None:
-        fname = fname + extension
+    return str(Path(basedir) / fname)
 
-    return pathjoin(basedir, fname)
+
+def assemble_geocolor_fname(
+    basedir,
+    product_name,
+    start_datetime,
+    color_key=None,
+    extension=".nc",
+):
+    """Produce GeoColor filenames matching Deb's Fortran code.
+
+    Format:
+        RAMMB_A2ECFG_GEOC_<COLOR>_<YYYYMMDD>_<HHMM>_T{tilenum}.nc
+    """
+    # Determine color code
+    color_letter = COLOR_MAPPING.get(color_key, "R")
+
+    # Extract date and time components
+    date_str = start_datetime.strftime("%Y%m%d")
+    time_str = start_datetime.strftime("%H%M")
+
+    fname = "_".join(
+        ["RAMMB_A2ECFG_GEOC", color_letter, date_str, time_str, "T{tilenum}"]
+    )
+
+    if extension:
+        fname += extension
+
+    return str(Path(basedir) / fname)
